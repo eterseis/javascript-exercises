6f1a6c6f90dbe680ccbc6d2b2df1170f18b43b14
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-07 22:53:03 -0300

    exercise 03 is done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (words) {
+  let string = "";
+  for (let i = 0; i < words.length; i++) {
+    string += words.at(words.length - 1 - i);
+  }
+  return string;
 };
 
 // Do not edit below this line
