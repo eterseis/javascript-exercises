66c477e07d0928e417ac61d0408765fc4f46e9f1
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-07 22:26:30 -0300

    exercise 02 is done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  if (num >= 0) {
+    let string = "";
+    for (let i = 0; i < num; i++) {
+      string += word;
+    }
+    return string;
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
