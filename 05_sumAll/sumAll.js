15e4d481eb382d8304350ae866296e44502767d7
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-11 16:17:14 -0300

    exercise 05 is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(...values) {
+    if (values.find((value) => value < 0)){
+        return "ERROR";
+    }
+    else if (values.find((value) => String(value).split("").indexOf(".") != -1)){
+        return "ERROR";
+    }
+    else if (values.find((value) => typeof value != "number")){
+        return "ERROR";
+    }
+    let counter = 0;
+    for (let i = Math.min.apply(null, values); i <= Math.max.apply(null, values); i++){
+        counter += i;
+    }
+    return counter;
 };
 
 // Do not edit below this line
