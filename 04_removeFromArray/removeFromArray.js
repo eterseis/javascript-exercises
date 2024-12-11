2d04e0f0b8a0ed60d3f4accddc57ac0833589a40
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-11 15:48:03 -0300

    exercise 04 is done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array = [], ...values) {
+  return array.filter((value) => values.indexOf(value) == -1);
 };
 
 // Do not edit below this line
