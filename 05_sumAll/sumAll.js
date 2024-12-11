be109cc3f40b2bf26fc3e95d3ec14604769488c5
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-11 16:23:16 -0300

    exercise 05 made better

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,12 +2,10 @@ const sumAll = function(...values) {
     if (values.find((value) => value < 0)){
         return "ERROR";
     }
-    else if (values.find((value) => String(value).split("").indexOf(".") != -1)){
+    else if (values.find((value) => Number.isInteger(value) == false)){
         return "ERROR";
     }
-    else if (values.find((value) => typeof value != "number")){
-        return "ERROR";
-    }
+    
     let counter = 0;
     for (let i = Math.min.apply(null, values); i <= Math.max.apply(null, values); i++){
         counter += i;
