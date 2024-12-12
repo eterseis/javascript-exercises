8208cc364d7360e268e9330e4c0fe12a639d3b98
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-11 21:39:05 -0300

    exercise 08 is done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (first, second) {
+  return (first += second);
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let multiplies = 1;
+  for (let i = 0; i < array.length; i++){
+    multiplies *= array[i];
+  }
+  return multiplies;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = 1;
+  for (let i = 0; i < num; i++){
+    factorial *= num-i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
@@ -29,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
