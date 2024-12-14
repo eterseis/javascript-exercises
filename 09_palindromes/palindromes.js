21c8f06aaeba964d4dbe7351d6604acd6ea5191b
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-13 23:14:18 -0300

    exercise 09 is done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const cleanedString = string
+    .toLowerCase()
+    .split("")
+    .filter((character) => validCharacters.includes(character))
+    .join("");
+
+  const reversedString = cleanedString.split('').reverse().join('');
+  const isPalindrome = cleanedString === reversedString;
+  return isPalindrome;
 };
-
+palindromes("Was it a Cat I Saw?");
 // Do not edit below this line
 module.exports = palindromes;
