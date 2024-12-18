430d8e0752a776ade1481d8e8219d2538feba56b
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-17 22:14:05 -0300

    fibonacci is done [10]

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+  if (member < 0) {
+    return "OOPS";
+  }
+  let fibo = [0, 1, 1];
+  for (let i = 3; fibo.length <= member; i++) {
+    fibo.push(fibo[i - 1] + fibo[i - 2]);
+  }
+  return fibo[member];
 };
 
 // Do not edit below this line
