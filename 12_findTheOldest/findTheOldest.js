8ea8c44d7b92b98304d82351c5801677f3850b76
Eterseis <148352159+eterseis@users.noreply.github.com>
2024-12-19 18:38:01 -0300

    exercise twelve is done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let person1 = people[people.length - 1];
+  let person2;
+  for (let i = 0; i < people.length; i++) {
+    person2 = people[i];
+    if (
+      typeof person2.yearOfDeath == "undefined" &&
+      person2.yearOfBirth < person1.yearOfBirth
+    ) {
+      return person2;
+    }
+    if (
+      person1.yearOfDeath - person1.yearOfBirth <
+      person2.yearOfDeath - person2.yearOfBirth
+    ) {
+      person1 = person2;
+    }
+  }
+  return person1;
 };
 
 // Do not edit below this line
